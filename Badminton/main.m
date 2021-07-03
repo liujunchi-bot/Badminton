e86ec6b33f041c7bcee68bf931c72c5b77ba3957
calmliu <liujunchigo@outlook.com>
2021-07-03 10:27:05 +0800

    Initial Commit

---
+++ Badminton/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Badminton
+//
+//  Created by calmliu on 2021/7/3.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
